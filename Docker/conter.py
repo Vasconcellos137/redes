1b9587d884a5a9f830a26a4d6e9c31eccfa431a5
Vasconcellos137 <vasconcellosvivih76@gmail.com>
2026-05-26 11:52:51 -0300

    26.5.26

---
+++ Docker/conter.py
@@ -0,0 +1,5 @@
+from time import sleep
+
+for i in range(10, 0, -1):
+    print(f"Nick - Python - {i}", flush=True)
+    sleep(1)
--- Docker/hi.sh
+++ Docker/hi.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-for i in 1 2 3 4 5 
+# for i in 1 2 3 4 5 
+#     do
+#         echo Nick: $i
+#         sleep 1s
+#     done
+
+# $ -> parâmetros q passamos p dentro do script
+# se n tiver esse echo... abaixo, n é possivel passar outros parâmetros 
+
+# echo $0 $1 $2 
+# for i in $(seq 5) 
+#     do
+#         echo Nick: $i
+#         sleep 1s
+#     done
+
+
+for i in $(seq $1 | tac ) 
     do
         echo Nick: $i
         sleep 1s
